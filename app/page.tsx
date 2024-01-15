7841b77d09f114377e6b1b6edd6e604b10199c24
Victor Olatunji <victoluolatunji@gmail.com>
2024-01-16 00:50:12 +0100

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Web note app</h1>
-    </main>
-  );
+export default function SignIn() {
+  return <main className='flex flex-col items-center justify-center'></main>;
 }
