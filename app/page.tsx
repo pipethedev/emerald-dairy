784a2ec325d586aca2bed700ff3ba5e744327904
Victor Olatunji <victoluolatunji@gmail.com>
2024-01-17 02:23:59 +0100

    [feat] quick links

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 import { H2 } from '@/utils/typography';
 import toast from 'react-hot-toast';
 import axios from 'axios';
-import { Info } from '@/components/svgs';
+import { Hearts, Info, MobileHearts, Verse } from '@/components/svgs';
 import Input from '@/components/input';
 import Button from '@/components/button';
 
@@ -95,16 +95,52 @@ export default function SignIn() {
         </form>
       </div>
 
-      <figure>
-        <img
-          src='/images/bimbo.png'
-          alt='bimbo'
-          // width={`100%`}
-          // height={100}
-          // priority
-          loading='lazy'
-          draggable={false}
-        />
+      <figure
+        className='col-span-3 text-[#FFFBF9] h-full bg-[url(/images/bimbo.png)] bg-right bg-no-repeat flex items-center justify-center flex-col w-full lg:w-auto'
+        style={{ backgroundSize: '95%' }}
+      >
+        <div className='flex w-[70%] flex-col items-start justify-center'>
+          <div className='gap-11 w-3/4 flex flex-col'>
+            <div className='flex flex-col gap-5'>
+              <span
+                className='flex w-[3rem] h-[3rem] p-2 items-center justify-center rounded-xl bg-[#FEF6F4] lg:bg-lg-heart lg:backdrop-blur-[6px]'
+                // style={{ background: "rgba(254, 246, 244, 0.12)", backdropFilter: "blur(6px)" }}
+              >
+                <span className='hidden lg:inline-block'>
+                  <Hearts />
+                </span>
+                <span className='lg:hidden'>
+                  <MobileHearts />
+                </span>
+              </span>
+              <p className='font-aeonik text-[7.5rem] leading-[6.5rem] font-bold tracking-[-0.375rem]'>
+                Emerald Diary
+              </p>
+            </div>
+            <div
+              className='rounded-2xl flex pr-2 items-center justify-start gap-4'
+              style={{
+                background: 'rgba(255, 255, 255, 0.06)',
+                backdropFilter: 'blur(6px)',
+              }}
+            >
+              <Verse />
+              <div>
+                <p className='font-aeonik text-[0.875rem] leading-[155%]'>
+                  <b className='font-bold text-[1.25rem] mb-2.5 block'>
+                    Verse of the Day
+                  </b>
+                  Behold, God is my salvation; I will trust, and will not be
+                  afraid; for the LORD GOD is my strength and my song, and he
+                  has become my salvation.
+                  <span className='font-medium text-[1rem] mt-2.5 block'>
+                    Isaiah 12 : 2
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </figure>
     </main>
   );
