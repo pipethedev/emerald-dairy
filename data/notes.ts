72c0c02c17dcebf39d5e288b84d73f49d8c6010b
Victor Olatunji <victoluolatunji@gmail.com>
2024-01-20 06:00:13 +0100

    [chore] notes resp

---
+++ data/notes.ts
@@ -0,0 +1,52 @@
+interface notesPreviewData {
+    title: string;
+    subtitle: string;
+    date: string,
+    tag: string,
+    favorite: boolean,
+}
+
+export const notesPreviewData: Array<notesPreviewData> = [
+    {
+        title: "Prayer as an Anchor",
+        subtitle: "Some note context come over to the second...",
+        date: '5, Dec 2023 • 4:58PM',
+        tag: 'Sermon',
+        favorite: false,
+    },
+    {
+        title: "Note Title goes here...",
+        subtitle: "Some note context come over to the second...",
+        date: '5, Dec 2023 • 4:58PM',
+        tag: 'Sermon',
+        favorite: true,
+    },
+    {
+        title: "Note Title goes here...",
+        subtitle: "Some note context come over to the second...",
+        date: '5, Dec 2023 • 4:58PM',
+        tag: 'Sermon',
+        favorite: true,
+    },
+    {
+        title: "Note Title goes here...",
+        subtitle: "Some note context come over to the second...",
+        date: '5, Dec 2023 • 4:58PM',
+        tag: 'Sermon',
+        favorite: true,
+    },
+    {
+        title: "Note Title goes here...",
+        subtitle: "Some note context come over to the second...",
+        date: '5, Dec 2023 • 4:58PM',
+        tag: 'Sermon',
+        favorite: true,
+    },
+    {
+        title: "Note Title goes here...",
+        subtitle: "Some note context come over to the second...",
+        date: '5, Dec 2023 • 4:58PM',
+        tag: 'Sermon',
+        favorite: true,
+    },
+]
