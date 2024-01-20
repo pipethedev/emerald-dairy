339e333f736692c6c457885e235b2804eb288cda
Victor Olatunji <victoluolatunji@gmail.com>
2024-01-20 12:37:58 +0100

    🤔

--- app/components/dashboard/verseOfDay.tsx
+++ app/components/dashboard/verseOfDay.tsx
@@ -1,4 +1,4 @@
-import { H5 } from '@/utils/typography';
+import { H5 } from "@/utils/typography";
 
 interface VerseOfDayProps {
   className?: string;
@@ -9,7 +9,7 @@ export default function VerseOfDay({ className }: VerseOfDayProps) {
     <div
       className={`${className} font-aeonikBold text-[#956E60] mx-3 bg-[#FEF6F4] md-h-[113px] md:w-[215px] rounded-md`}
     >
-      <H5 className='pl-3 pb-[80px] md:pt-4 '>Verse of the Day</H5>
+      <H5 className="pl-3 pb-[80px] md:pt-4 ">Verse of the Day</H5>
     </div>
   );
 }
